d23897e38149cf79612792e800c3c7e1c0b7f213
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 20:24:51 -0400

    (feat) add navigation links to header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import { Link } from 'react-router';
 
 import { headerStyle } from '../styles';
 
 const Header = () => (
   <div>
-    <div style={headerStyle}> This is the Header! </div>
+    <div style={headerStyle}>
+      <Link to="/"> Index </Link>
+      <Link to="/users"> Users </Link>
+    </div>
     <br />
   </div>
 );
